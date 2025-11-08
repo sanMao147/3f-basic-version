432cd12d8607f97388bf244b44986ae70748f79d
wuhol <charlielin007@outlook.com>
2025-11-08 22:20:10 +0800

    feat: add three block

--- src/components/Level.tsx
+++ src/components/Level.tsx
@@ -79,12 +79,87 @@ const BlockSpinner = ({ position = [0, 0, 0] }: BlockProps) => {
     </>
   )
 }
+const BlockLimbo = ({ position = [0, 0, 0] }: BlockProps) => {
+  const obstacleRef = useRef<RapierRigidBody>(null!)
+  const [timeOffset] = useState(() => Math.random() * Math.PI * 2)
+  useFrame((state) => {
+    if (!obstacleRef.current) return
+    const time = state.clock.elapsedTime
+    const y = Math.sin(time + timeOffset) + 1.15
+    obstacleRef.current.setNextKinematicTranslation({
+      x: position[0],
+      y,
+      z: position[2],
+    })
+  })
 
+  return (
+    <>
+      <group position={position}>
+        <Box
+          size={[4, 0.2, 4]}
+          color='yellow'
+        />
+        <RigidBody
+          ref={obstacleRef}
+          type='kinematicPosition'
+          position={[0, 0.3, 0]}
+          restitution={0.2}
+          friction={0}
+        >
+          <Box
+            size={[3.5, 0.5, 0.2]}
+            color='red'
+          />
+        </RigidBody>
+      </group>
+    </>
+  )
+}
+const BlockAxe = ({ position = [0, 0, 0] }: BlockProps) => {
+  const obstacleRef = useRef<RapierRigidBody>(null!)
+  useFrame((state) => {
+    if (!obstacleRef.current) return
+    const time = state.clock.elapsedTime
+    const x = Math.sin(time) * 1.25
+    obstacleRef.current.setNextKinematicTranslation({
+      x,
+      y: position[1] + 1.5,
+      z: position[2],
+    })
+  })
+
+  return (
+    <>
+      <group position={position}>
+        <Box
+          size={[4, 0.2, 4]}
+          color='yellow'
+        />
+        <RigidBody
+          ref={obstacleRef}
+          type='kinematicPosition'
+          position={[0, 0.3, 0]}
+          restitution={0.2}
+          friction={0}
+        >
+          <Box
+            size={[1.5, 2.5, 0.3]}
+            color='red'
+          />
+        </RigidBody>
+      </group>
+    </>
+  )
+}
 export const Level = () => {
   return (
     <>
-      <BlockStart position={[0, 0, 4]} />
-      <BlockSpinner position={[0, 0, 0]} />
+      <BlockStart position={[0, 0, 16]} />
+      <BlockSpinner position={[0, 0, 12]} />
+      <BlockLimbo position={[0, 0, 8]} />
+      <BlockAxe position={[0, 0, 4]} />
+      <BlockStart position={[0, 0, 0]} />
     </>
   )
 }
