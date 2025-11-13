1300978dd1b65711307aadaca3b37cdc54926f44
wuhol <charlielin007@outlook.com>
2025-11-13 20:20:51 +0800

    feat: auto deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/3f-basic-version/',
   plugins: [react(), tailwindcss()],
 })
