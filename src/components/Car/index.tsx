e239f2d9d75bbc431d8197d02a9c18bc9842842c
wuhol <charlielin007@outlook.com>
2025-11-26 12:15:42 +0800

    feat: car race

--- src/components/Car/index.tsx
+++ src/components/Car/index.tsx
@@ -1,3 +1,7 @@
 export default function Car() {
-  return <>car</>
+  return (
+    <div className='text-white flex items-center justify-center  text-9xl h-screen'>
+      Car Race Comming Soon
+    </div>
+  )
 }
--- src/pages/CarRace/index.tsx
+++ src/pages/CarRace/index.tsx
@@ -1,7 +1,4 @@
+import CarBasic from '@/components/Car'
 export default function Car() {
-  return (
-    <div className='text-white flex items-center justify-center  text-9xl h-screen'>
-      Car Race
-    </div>
-  )
+  return <CarBasic />
 }
